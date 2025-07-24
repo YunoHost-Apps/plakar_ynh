01e5ecccb3d6c4422c04af2c8ebc486dcc149735
lapineige <lapineige@users.noreply.github.com>
2025-07-25 00:10:54 +0200

    fix syntax

--- conf/plakar.sh
+++ conf/plakar.sh
@@ -1,1 +1,1 @@
-cd __DATA_DIR__ && PLAKAR_PASSPHRASE=__ENCRYPTION_PASSPHRASE__ __INSTALL_DIR__/plakar backup -tag automated-cron-backup -check 
+PLAKAR_PASSPHRASE=__ENCRYPTION_PASSPHRASE__ __INSTALL_DIR__/plakar at __DATA_DIR__ backup -tag automated-cron-backup -check 
