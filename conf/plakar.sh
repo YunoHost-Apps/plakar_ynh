ab7d750bf639f27b5b4c321024c340835fa0bcde
lapineige <lapineige@users.noreply.github.com>
2025-07-25 00:35:42 +0200

    use global plakar command
    Should be reverted when we will finally use the local install…

--- conf/plakar.sh
+++ conf/plakar.sh
@@ -1,1 +1,1 @@
-PLAKAR_PASSPHRASE=__ENCRYPTION_PASSPHRASE__ __INSTALL_DIR__/plakar at __DATA_DIR__ backup -tag automated-cron-backup -check 
+PLAKAR_PASSPHRASE=__ENCRYPTION_PASSPHRASE__ plakar at __DATA_DIR__ backup -tag automated-cron-backup -check 
