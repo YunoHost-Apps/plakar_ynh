ccb21bf113be7f34845a3bdb605ac79c8c7464ee
lapineige <lapineige@users.noreply.github.com>
2025-07-30 08:45:39 +0200

    Fix choices syntax (#5)

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,33 +7,33 @@
 _set_frequencies() {
 
     case $frequency_hour in
-      "Midnight")          frequency_time="0" ; ynh_app_setting_set --app="$app" --key="frequency_time" --value="$frequency_time" ;;
-      "2am")               frequency_time="2" ; ynh_app_setting_set --app="$app" --key="frequency_time" --value="$frequency_time" ;;
-      "3am")               frequency_time="3" ; ynh_app_setting_set --app="$app" --key="frequency_time" --value="$frequency_time" ;;
-      "4am")               frequency_time="4" ; ynh_app_setting_set --app="$app" --key="frequency_time" --value="$frequency_time" ;;
-      "6am")               frequency_time="6" ; ynh_app_setting_set --app="$app" --key="frequency_time" --value="$frequency_time" ;;
+      "Midnight")          frequency_hour="0" ; ynh_app_setting_set --app="$app" --key="frequency_hour" --value="$frequency_hour" ;;
+      "2am")               frequency_hour="2" ; ynh_app_setting_set --app="$app" --key="frequency_hour" --value="$frequency_hour" ;;
+      "3am")               frequency_hour="3" ; ynh_app_setting_set --app="$app" --key="frequency_hour" --value="$frequency_hour" ;;
+      "4am")               frequency_hour="4" ; ynh_app_setting_set --app="$app" --key="frequency_hour" --value="$frequency_hour" ;;
+      "6am")               frequency_hour="6" ; ynh_app_setting_set --app="$app" --key="frequency_hour" --value="$frequency_hour" ;;
     esac
      
     case "$frequency" in
         daily)
-            frequency_cron="0 $frequency_time * * *"
-            frequency_human="every day at $frequency_time am"
+            frequency_cron="0 $frequency_hour * * *"
+            frequency_human="every day at $frequency_hour am"
             ;;
         days_3)
-            frequency_cron="0 $frequency_time */3 * *"
-            frequency_human="each 3 days at $frequency_time am"
+            frequency_cron="0 $frequency_hour */3 * *"
+            frequency_human="each 3 days at $frequency_hour am"
             ;;
         weekly)
-            frequency_cron="0 $frequency_time * * 0"
-            frequency_human="once a week on sunday at $frequency_time am"
+            frequency_cron="0 $frequency_hour * * 0"
+            frequency_human="once a week on sunday at $frequency_hour am"
             ;;
         weeks_2)
-            frequency_cron="0 $frequency_time * * 0/2"
-            frequency_human="one sunday out of two at $frequency_time am"
+            frequency_cron="0 $frequency_hour * * 0/2"
+            frequency_human="one sunday out of two at $frequency_hour am"
             ;;
         monthly)
-            frequency_cron="0 $frequency_time 1 * *"
-            frequency_human="once a month on the first sunday at $frequency_time am"
+            frequency_cron="0 $frequency_hour 1 * *"
+            frequency_human="once a month on the first sunday at $frequency_hour am"
             ;;
         *)
             ynh_die "Unsupported frequency $frequency" ;;
